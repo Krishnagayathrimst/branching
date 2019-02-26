e992d4444ce526c6a43191e03aac9c39bb06bea4
krishnagayathrimst <krishnagayathri.g@mstsolutions.com>
2019-02-26 12:43:33 +0530

    Addind a reverse java program to feature/Sprint_1 branch

---
+++ Reverse.java
@@ -0,0 +1,20 @@
+class Reverse
+{
+	public static void main(String[] args)
+	{
+		int number=299;
+		int mod,reverse=0;
+		int palindrome=number;
+		while(number>0)
+		{
+			mod=number%10;
+			reverse=reverse*10+mod;
+			number=number/10;
+		}
+		System.out.println("The Reversed number is "+reverse);
+		if(palindrome==reverse)
+		{
+			System.out.println("The given number is palindrome");
+		}
+	}
+}
